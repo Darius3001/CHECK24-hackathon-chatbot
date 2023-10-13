06e9a3965cbf5dbd637900aacfa9bfeea535a9f8
Darius Mertin <darius.mertin@check24.de>
2023-10-13 18:36:55 +0200

    working test

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-import { ChatGPTAPI } from "chatgpt";
+const OpenAI = require('openai')
 
-const api = new ChatGPTAPI({
+require('dotenv').config()
+
+const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
 })
 
+async function main() {
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(chatCompletion.choices);
+}
+
+main()
